cf787ff799acb519506930cc9955b953e111e71a
Davies Brown <nwosudavid77@gmail.com>
2024-03-19 20:51:25 +0100

    basic flask app

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Starts Vercel Web Application """
+from os import environ
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
