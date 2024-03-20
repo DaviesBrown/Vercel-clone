3a5fdec5530e11025a4cb2cb001151a21e6ced3f
Davies Brown <nwosudavid77@gmail.com>
2024-03-20 03:34:00 +0100

    added a deploy route and function to get all files in a directory

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,17 @@
+#!/usr/bin/python3
+import os
+from typing import List
 from uuid import uuid4
 
 def generate() -> str:
     """ generates random id for the session """
     return str(uuid4())
+
+def get_all_files(path: str) -> List[str]:
+    """ gets all the paths of files stored in out/id dir """
+    paths: list[str] = []
+    for dir, subdirs, files in os.walk(path):
+        for file in files:
+            file_path = f"{dir}/{file}"
+            paths.append(file_path)
+    return paths
