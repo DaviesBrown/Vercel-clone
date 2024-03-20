c74dc64b421d42b74bffd37fdb460d1f69e64d94
Davies Brown <nwosudavid77@gmail.com>
2024-03-20 23:39:14 +0100

    moved current files to upload folder to dockerize

---
+++ upload-service/api/v1/views/deploy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Deploy """
+from api.v1.views import app_views
+from api.utils import generate, get_all_files, upload_file_to_S3
+from api.redis_queue import Queue
+from flask import jsonify, request
+import git
+import os
+
+redis_publisher = Queue()
+
+
+@app_views.route('/deploy', methods=['GET'], strict_slashes=False)
+def deploy():
+    """ Deploying production code """
+    repo_url = request.args.get('repo_url')
+    session_id = generate()
+    path = os.getcwd()
+    path = os.path.split(path)[0]
+    git.Repo.clone_from(repo_url, f"{path}/out/{session_id}")
+    file_paths = get_all_files(f"out/{session_id}")
+    # new_repo = git.Repo(repo_url) /local deployment
+    # new_repo.clone(f"{path}/out/{session_id}")
+    map(upload_file_to_S3, file_paths)
+    redis_publisher.push("upload_id", session_id)
+    return jsonify({"status": "files uploaded", "id": session_id})
