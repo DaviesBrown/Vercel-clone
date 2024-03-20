32e6080169249ee89bfabd91738f3ae191b8912c
Davies Brown <nwosudavid77@gmail.com>
2024-03-20 10:15:27 +0100

    uploads file to S3 bucket

--- api/utils.py
+++ api/utils.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
+import boto3
+from boto3.s3 import transfer
+from botocore.exceptions import ClientError
+import git
+import logging
 import os
 from typing import List
 from uuid import uuid4
 
+
 def generate() -> str:
     """ generates random id for the session """
     return str(uuid4())
+
+
+def get_transfer_client() -> transfer.S3Transfer:
+    """ returns a transfer client """
+    s3_client = boto3.client("s3")
+    transfer_client = transfer.S3Transfer(s3_client)
+    return transfer_client
 
 def get_all_files(path: str) -> List[str]:
     """ gets all the paths of files stored in out/id dir """
@@ -15,3 +28,16 @@ def get_all_files(path: str) -> List[str]:
             file_path = f"{dir}/{file}"
             paths.append(file_path)
     return paths
+
+def upload_file_to_S3(filepath: str, bucket: str) -> bool:
+    """ Uploads file to S3 given a path """
+    object_name = os.path.basename(filepath)
+    try:
+        transfer_client = get_transfer_client()
+        transfer_client.upload_file(filepath, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+upload_file_to_S3('/home/brownian77/christmas-card/index.html', 'hello')
