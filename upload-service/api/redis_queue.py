2fc07b811b7d3fedd8b1f7762a254f4d739b8f62
Davies Brown <nwosudavid77@gmail.com>
2024-03-23 15:01:49 +0100

    feat: added getting and setting status in redis

--- upload-service/api/redis_queue.py
+++ upload-service/api/redis_queue.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 import redis
 
+
 class Queue:
     def __init__(self) -> None:
         self.publisher = redis.Redis()
-    
+
     def push(self, name: str, id: str):
         self.publisher.lpush(name, id)
+
+    def get(self, id: str):
+        return self.publisher.hget("status", id)
+
+    def set(self, id: str, item: str):
+        self.publisher.hset("status", id, item)
