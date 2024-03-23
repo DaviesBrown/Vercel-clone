69c2f701e368ce425f315d6e5c17f89a71350d39
Davies Brown <nwosudavid77@gmail.com>
2024-03-23 15:04:15 +0100

    fix: adjusted deploy for local and fixed status route

--- upload-service/api/v1/views/deploy.py
+++ upload-service/api/v1/views/deploy.py
@@ -2,12 +2,10 @@
 """ Deploy """
 from api.v1.views import app_views
 from api.utils import generate, get_all_files, upload_file_to_S3
-from api.redis_queue import Queue
+from api import redis_pub
 from flask import jsonify, request
 import git
 import os
-
-redis_publisher = Queue()
 
 
 @app_views.route('/deploy', methods=['GET'], strict_slashes=False)
@@ -17,10 +15,11 @@ def deploy():
     session_id = generate()
     path = os.getcwd()
     path = os.path.split(path)[0]
-    git.Repo.clone_from(repo_url, f"{path}/out/{session_id}")
-    file_paths = get_all_files(f"out/{session_id}")
-    # new_repo = git.Repo(repo_url) /local deployment
-    # new_repo.clone(f"{path}/out/{session_id}")
-    map(upload_file_to_S3, file_paths)
-    redis_publisher.push("upload_id", session_id)
+    #git.Repo.clone_from(repo_url, f"{path}/out/{session_id}")
+    #file_paths = get_all_files(f"out/{session_id}")
+    new_repo = git.Repo(repo_url) #local deployment
+    new_repo.clone(f"{path}/out/{session_id}")
+    #map(upload_file_to_S3, file_paths)
+    redis_pub.push("build-queue", session_id)
+    redis_pub.set(session_id, "uploaded")
     return jsonify({"status": "files uploaded", "id": session_id})
--- upload-service/api/v1/views/index.py
+++ upload-service/api/v1/views/index.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ Index """
 from api.v1.views import app_views
-from flask import jsonify
+from api import redis_sub
+from flask import jsonify, request
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Status of API """
-    return jsonify({"status": "OK"})
+    """ Status of deployment """
+    id = request.args.get("id")
+    response: str = redis_sub.get(id).decode("utf-8")
+    return jsonify({"status": response})
